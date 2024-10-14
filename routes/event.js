000031f266754f8b4c2573db2191dacf227ae2df
Dexshine <b_o_y_s@hotmail.com>
2024-10-14 09:31:30 +0700

    add route to  retrieve events created by a specific user

--- routes/event.js
+++ routes/event.js
@@ -8,10 +8,12 @@ const {
   getEventById,
   updateEventById,
   deleteEventById,
+  getEventsByCreatedBy,
 } = require("../controllers/event");
 
 router.post("/", createEvent);
 router.get("/", getAllEvents);
+router.get("/createdBy/:id", getEventsByCreatedBy);
 router.get("/:id", getEventById);
 router.put("/:id", updateEventById);
 router.delete("/:id", deleteEventById);
