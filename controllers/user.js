8ccbf939ad02ba9f03196b3bd056c9ce51d235fe
Dexshine <b_o_y_s@hotmail.com>
2024-10-14 21:23:04 +0700

    express route handler that retrieves a list of all users

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,18 @@
 const User = require("../models/user");
 
+exports.index = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      message: "Users retrieved successfully",
+      data: users,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
 exports.show = async (req, res) => {
   try {
     const id = req.params.id;
